f646e16b561649464fb864b72d5e18afacae7ecd
Sneha-vocab <sneha-vocab@gmail.com>
2025-12-22 11:09:06 +0530

    Added logger.py - Logging utility

---
+++ app/utils/logger.py
@@ -0,0 +1,16 @@
+"""Logging configuration."""
+import logging
+import sys
+from app.config import settings
+
+# Configure logging
+logging.basicConfig(
+    level=logging.DEBUG if settings.debug else logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.StreamHandler(sys.stdout),
+    ]
+)
+
+logger = logging.getLogger("lnt_finance_feedback")
+
